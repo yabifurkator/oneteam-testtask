af315c56b2861a5b339d071f1d852cfbf4eb0d6b
Leonid <yabifurkator@pop-os.localdomain>
2023-01-11 05:10:06 +0300

    new double-click feature added

--- index.php
+++ index.php
@@ -182,7 +182,7 @@ table, th, td {
 
 .column {
     float: left;
-    width: 50%;
+    width: 25%;
 }
 
 .row:after {
@@ -219,21 +219,36 @@ table {
 
 <script type="text/javascript">
 $("td").live('click', function() {
-    var class_name = $(this).attr('class');
+    var class_name = $(this).attr('class').split(' ')[0];
+
     $("tr").removeClass('match-selected');
+    $("td").removeClass('match-selected');
     $("td").not(this).removeClass('selected');
+    $("td").not(this).removeClass('double-clicked');
 
-    $("tr").filter('.' + class_name).addClass('match-selected');
-    $(this).toggleClass('selected');
+    if (!$(this).hasClass('selected')) {
+        $(this).addClass('selected');
+        $("tr").filter('.' + class_name).addClass('match-selected');
+    }
+    else if(!$(this).hasClass('double-clicked')) {
+        $("tr").removeClass('match-selected');
+        $("td").filter('.' + class_name).not(this).addClass('match-selected');
+        $(this).addClass('double-clicked');
+    }
+    else {
+        $("tr").removeClass('match-selected');
+        $("td").removeClass('match-selected');
+        $("td").removeClass('selected');
+        $("td").removeClass('double-clicked');
+    }
     console.log(class_name);
-
 });
 </script>
 
 </head>
 <body>
     <div class="row">
-        <div class="column" style="background-color:#90EE90;">
+        <div class="column" style="background-color:#90EE90; margin-left: 400px">
             <h3>ТУРЫ ПЕРВОГО КРУГА</h3>
             <?php
             foreach ($tours as $tour_index => $tour) {
